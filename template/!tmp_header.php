85a7c53f1906b2f48caf91d462a5bbec844a8a02
Andrey Galushko <createsite2016@gmail.com>
2016-11-08 17:27:29 +0300

    Шаблон шапки
    Тут шрифты и css, а так же заголовок

---
+++ template/!tmp_header.php
@@ -0,0 +1,10 @@
+<html lang="ru">
+  <head>
+       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+       <link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.indigo-pink.min.css">
+       <link rel="stylesheet" href="css/main.css">
+       <script defer src="https://code.getmdl.io/1.2.1/material.min.js"></script>
+       
+       <title><?php echo $title;?></title>
+  </head>
+  <body>
