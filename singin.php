2fd756e70bcdc2adc58138d37de6586ec1414f7c
Andrey Galushko <createsite2016@gmail.com>
2016-11-08 17:30:18 +0300

    Страница Регистрации пользователя
    Обычная форма и снузу подключены файлы шаблонов

---
+++ singin.php
@@ -0,0 +1,44 @@
+<?php
+// Входные данные
+        $ip = $_SERVER["REMOTE_ADDR"];
+// Контент страницы
+        $title = "Регистрация пользователя"; // Заголовок
+// Название и пункты меню
+        $en_menu = 'hide'; // для показа меню есть два свойства show или hide.
+        $title_menu = " "; // заголовок меню
+        $title_content_page = "Регистрация пользователя";
+        $content_page = "
+        <center>
+            <form action='classes/App.php' method='POST'>
+              <div class='mdl-textfield mdl-js-textfield mdl-textfield--floating-label'>
+                <input class='mdl-textfield__input' type='text' name='fname' required>
+                <label class='mdl-textfield__label' for='sample3'>Имя пользователя(только русскими буквми)</label>
+              </div>
+              <br>
+              <div class='mdl-textfield mdl-js-textfield mdl-textfield--floating-label'>
+                <input class='mdl-textfield__input' type='text' name='flogin' required> 
+                <label class='mdl-textfield__label' for='sample3'>Логин(только латинскими буквами)</label>
+              </div>
+              <br>
+              <div class='mdl-textfield mdl-js-textfield mdl-textfield--floating-label'>
+                <input class='mdl-textfield__input' type='password' name='fpassword' required>
+                <label class='mdl-textfield__label' for='sample3'>Пароль(минимум 6 символов)</label>
+                <input type='hidden' name='ip' value='".$ip."'> 
+                <input type='hidden' name='act' value='singin'>
+              </div>
+              <br>
+              <button class='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent'>
+                  Зарегистрировать
+              </button>
+            </form>
+        </center>
+
+        ";
+
+// Подключение шаблона
+        include("template/!tmp_header.php"); // хэдер
+        include("template/!tmp_menu.php"); // меню
+        include("template/!tmp_content.php"); // контент страницы
+        include("template/!tmp_footer.php"); // футер
+
+?>
