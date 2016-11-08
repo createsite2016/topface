a64b21d9268a42b33c30eac4b584b32b4ede9153
Andrey Galushko <createsite2016@gmail.com>
2016-11-08 22:46:11 +0300

    Динамическое меню
    В зависимости авторизован пользователь или нет оно меняется

--- template/!tmp_menu.php
+++ template/!tmp_menu.php
@@ -1,8 +1,18 @@
 <?php
+session_start();
 
-        $link1 = "index.php"; $link1_title = "Главная";
-        $link2 = "users.php"; $link2_title = "Пользователи";
-        $link3 = "exit.php"; $link3_title = "Выход";
+// проверяем есть ли данные о пользователе
+if (!empty($_SESSION['login']) or !empty($_SESSION['id'])) {
+    $en_menu = 'show';
+    $title_menu = "Привет, " . $_SESSION['name']; // приветствуем пользователя
+    $link1 = "index.php"; $link1_title = "Главная";
+    $link2 = "admin.php"; $link2_title = "Пользователи";
+    $link3 = "classes/app.php?act=exit"; $link3_title = "Выход";
+}
+else {
+    $en_menu = 'hide'; // для показа меню есть два свойства show или hide.
+}
+
 
 ?>
 
@@ -15,9 +25,15 @@
             <div class="mdl-layout-spacer"></div>
             <!-- Navigation. We hide it in small screens. -->
             <nav class="mdl-navigation mdl-layout--large-screen-only">
-            <a class="mdl-navigation__link" href="../index.php">Главная</a>
-              <a class="mdl-navigation__link" href="../login.php">Вход</a>
-              <a class="mdl-navigation__link" href="../singin.php">Регистрация</a>
+              <?php
+              if (empty($_SESSION['login']) or empty($_SESSION['id'])) { ?>
+
+                    <a class="mdl-navigation__link" href="../index.php">Главная</a>
+                    <a class="mdl-navigation__link" href="../login.php">Вход</a>
+                    <a class="mdl-navigation__link" href="../singin.php">Регистрация</a>
+
+              <?php } ?>
+
             </nav>
           </div>
         </header>
